e0d06b62eca2909c396cf4c226c8c5b9f9103a8b
Aditya Kumar <padityak79@gmail.com>
2019-12-01 17:36:26 +0530

    Changed hit.cpp

---
+++ hit.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int gcd(long long int a, long long int b){
+    if(a==0) return b;
+    return gcd(b%a,a);
+}
+int main(){
+long long int a=53,b=106;
+cout<<gcd(a,b);
+}
